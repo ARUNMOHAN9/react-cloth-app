756628cbc45502ec3672f080b23176c349ab95e4
Arun Mohan <arunmh7@gmail.com>
2021-02-19 23:55:39 +0530

    added category navigation

--- src/modules/shop/components/collectionPreview/CollectionPreview.component.tsx
+++ src/modules/shop/components/collectionPreview/CollectionPreview.component.tsx
@@ -1,6 +1,7 @@
 
 import { Container, Grid } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { IProductCategory } from '../../../shared/interfaces/product-item.interface';
 import CollectionItem from '../collectionItem/CollectionItem.component';
 
@@ -10,19 +11,25 @@ interface IProps extends IProductCategory {
 
 }
 
-const CollectionPreview = ({ title, items }: Omit<IProps, 'id'>) => (
-    <Container maxWidth="xl" className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <Grid container spacing={2}>
-            {items
-                .filter((_, idx) => idx < 4)
-                .map((item) => (
-                    <Grid item xs={12} sm={6} md={3} key={item.id}>
-                        <CollectionItem item={item} />
-                    </Grid>
-                ))}
-        </Grid>
-    </Container>
-);
+const CollectionPreview = ({ title, items, routeName }: Omit<IProps, 'id'>) => {
+
+    const link = `shop/${routeName}`;
+    return (
+        <Container maxWidth="xl" className='collection-preview'>
+            <h1 className='title'>
+                <Link to={link}>{title.toUpperCase()}</Link>
+            </h1>
+            <Grid container spacing={2}>
+                {items
+                    .filter((_, idx) => idx < 4)
+                    .map((item) => (
+                        <Grid item xs={12} sm={6} md={3} key={item.id}>
+                            <CollectionItem item={item} />
+                        </Grid>
+                    ))}
+            </Grid>
+        </Container >
+    );
+};
 
 export default CollectionPreview;
--- src/modules/shop/pages/categoryPage/CategoryPage.component.tsx
+++ src/modules/shop/pages/categoryPage/CategoryPage.component.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import { Container, Grid } from '@material-ui/core';
+import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import SHOP_DATA from '../../../../assets/data/shop.data';
+
+import CollectionItem from '../../components/collectionItem/CollectionItem.component';
 
 import './CategoryPage.styles.scss';
 
-const CategoryPage = () => {
+interface MatchParams {
+    categoryId: string;
+}
 
-    return (
-        <div className='shop-page'>
-            categorypage
-        </div>
-    );
+interface IProps extends RouteComponentProps<MatchParams> {
+}
+
+
+const CategoryPage = ({ match: { params } }: IProps) => {
+
+    const { categoryId } = params;
+
+    const selectedCategory = SHOP_DATA.find(elem => elem.routeName === categoryId);
+
+    let result = <div>No items found</div>;
+
+    if (selectedCategory && selectedCategory?.items?.length) {
+        result = (
+            <Container maxWidth="xl">
+                <Grid container spacing={2}>
+                    {
+                        selectedCategory.items.map(item => <Grid item xs={12} sm={6} md={4} lg={3}><CollectionItem item={item} /></Grid>)
+                    }
+                </Grid>
+            </Container>
+        );
+    }
+
+    return result;
 };
 
 export default CategoryPage;
