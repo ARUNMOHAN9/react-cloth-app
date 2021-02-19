45c25acdc0de2d41d9eab4e2ff3a69306fad37cc
Arun Mohan <arunmh7@gmail.com>
2021-02-20 00:56:35 +0530

    stripe update and minor fixes

--- src/modules/checkout/pages/checkoutPage/CheckoutPage.component.tsx
+++ src/modules/checkout/pages/checkoutPage/CheckoutPage.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { IStateProductItem } from '../../../../redux/cart/cart-reducer.interface';
 import { IState } from '../../../../redux/root-reducer.interface';
+import StripeBtn from '../../../shared/components/stripe-button/StripeBtn.component';
 import CheckoutItem from '../../components/checkout-item/CheckoutItem';
 
 import './CheckoutPage.styles.scss';
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, totalPrice }: IProps) => {
                 <CheckoutItem key={cartItem.id} cartItem={cartItem} />
             ))}
             <div className='total'>TOTAL: ${totalPrice}</div>
+            <div className='test-warning'>
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+            </div>
+            <StripeBtn price={totalPrice} />
         </div>
     )
 }
---
+++ src/modules/shared/components/stripe-button/StripeBtn.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeBtn = ({ price }: { price: number }) => {
+
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_mHAVxe0nbcv3gI33LJw9KeZ400CwRKkU1G';
+
+    const onToken = (token: any) => {
+        console.log(token);
+        alert('Payment successful');
+    }
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            stripeKey={publishableKey}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+        />
+    )
+}
+
+export default StripeBtn;
